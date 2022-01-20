bdc63a645622e1510c53769d036376308adbf7fa
Timur Svoboda <svobodatimur@gmail.com>
2022-01-20 09:40:54 +0200

    (command dev): Add a local server with live reload

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,25 @@
 import gulp from "gulp";
+import BrowserSync from "browser-sync";
+const bs = BrowserSync.create();
+import colors from "colors/safe.js";
 
-export default function(cb) {
-  console.log("Hello, world!");
+const config = {
+  paths: {
+    server: "./src"
+  }
+};
+
+export function dev(cb) {
+  bs.init({
+    server: config.paths.server
+  });
+
+  bs.watch(config.paths.server).on("change", path => {
+    console.log(`File ${colors.green(path)} has been changed`);
+    bs.reload();
+  });
+
   cb();
 }
+
+export default dev;
